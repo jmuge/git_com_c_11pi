2388a3fc5f7410ca81fad6d0f2e4f2b4cdb2d2d6
jmuge <189981839+jmuge@users.noreply.github.com>
2025-10-21 19:59:12 +0100

    Criar ficheiro em C
    Com leitura de dois inteiros, para começar.

---
+++ c_calcular.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+    // introduzir dois valores inteiros
+    int n1, n2;
+    printf("Introduza dois valores inteiros separados por espaço: ");
+    scanf("%d %d", &n1, &n2);
+
+    //printf("%d\n", n1);
+    //printf("%d\n", n2);
+
+    return 0;
+
+
+}
