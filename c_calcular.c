1fff36ac6c99112d67c2493faa699baea75a0454
jmuge <189981839+jmuge@users.noreply.github.com>
2025-10-22 19:08:57 +0100

    Adicionar função para somar e chamar a mesma

--- c_calcular.c
+++ c_calcular.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-
+// função para calcular a soma de dois inteiros
+int soma(int a, int b){
+    return a + b;
+}
 
 int main(){
 
@@ -11,6 +14,9 @@ int main(){
 
     //printf("%d\n", n1);
     //printf("%d\n", n2);
+
+    // chamar as funções para calcular a soma, subtração, multiplicação e divisão
+    int resultado_soma = soma(n1, n2);
 
     return 0;
 
